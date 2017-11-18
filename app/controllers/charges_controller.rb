11b62e4d42a1f6dbb5b0046728854ef6778578c2
PRIYAM PATEL <patelpriyam203@gmail.com>
2017-11-18 18:14:17 -0500

    Checkpoint-7-rails-blocipedia-upgradinganaccount: Completed

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -15,15 +15,17 @@ class ChargesController < ApplicationController
      currency: 'usd'
    )
 
+   current_user.premium!
+
    flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
-   redirect_to user_path(current_user) # or wherever
+   redirect_to root_path # or wherever
 
    # Stripe will send back CardErrors, with friendly messages
    # when something goes wrong.
    # This `rescue block` catches and displays those errors.
    rescue Stripe::CardError => e
      flash[:alert] = e.message
-     redirect_to new_charge_path
+     redirect_to new_charges_path
   end
 
 
